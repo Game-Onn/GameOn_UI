9c3018485d87ffb239eff8c120742d7f66298b45
shantanu-kukade <srkukade@mitaoe.ac.in>
2025-04-01 23:07:24 +0530

    GOUI-3 Added styling to Login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,12 +1,39 @@
+import { Container, Row, Col, Form, Button, Image } from 'react-bootstrap';
+import gameon from '../image/Game.png'
+
+
 function Login() {
     return (
-        <div className="login">
-            <img src="../logo.svg" className="mini-logo" alt="minilogo" />
-            <input type="text" name="uid" placeholder="Phone No."/>
-            <input type="password" name="password" placeholder="Password"/>
-            <button type="submit" name="Login"> Login </button>
-        </div>
-    )
+        <Container>
+            <Row>
+                <Col></Col>
+                <Col>
+                    <Container className='pt-3 '>
+                        <Image 
+                        src={gameon}
+                        height="200px"
+                        rounded />
+                    </Container>
+                </Col>
+                <Col>
+                    <Container className='pt-5 pb-5 bg-dark text-white'>
+                        <Form>
+                            <Form.Group className='mb-3' controlId='exampleForm.ControlInput1'>
+                                <Form.Control type='number' placeholder='Phone Number' name='username'/>
+                            </Form.Group>
+                            <Form.Group className='mb-3' controlId='exampleForm.ControlTextarea1'>
+                                <Form.Control type='password' placeholder='Passowrd' name='password'/>
+                            </Form.Group>
+                            <Form.Group  controlId='exampleForm.ControlTextarea1'>
+                                <Button variant='secondary'  type='submit'> Login </Button>
+                            </Form.Group>
+                        </Form>
+                    </Container>
+                </Col>
+                <Col></Col>
+            </Row>
+        </Container>
+    );
 }
 
 export default Login;
